6f36dcad0bea908bb7cfb93da51d5784367b9ceb
monaum.202 <monaum.202@gmail.com>
2025-09-09 17:58:55 +0600

    1414

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { InventoryComponent } from './inventory/inventory.component';
 import { DashboardComponent } from './inventory/dashboard/dashboard.component';
 import { AllOrdersComponent } from './inventory/orders/all-orders/all-orders.component';
+import { SideNavComponent } from './inventory/side-nav/side-nav.component';
+import { CustomerComponent } from './inventory/customer/customer.component';
+import { SupplierComponent } from './inventory/supplier/supplier.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/inventory', pathMatch: 'full' },
   { path: 'inventory',component: InventoryComponent,children: [
     { path: '', component: DashboardComponent },
     {path: 'dashboard', component: DashboardComponent},
-    {path: 'all-orders',component: AllOrdersComponent}]
+    {path: 'all-orders',component: AllOrdersComponent},
+    {path: 'side-bar',component:SideNavComponent},
+    {path: 'customers',component:CustomerComponent},
+  {path: 'suppliers',component:SupplierComponent}]
   },
   { path: '**', redirectTo: '/inventory' }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { DashboardComponent } from './inventory/dashboard/dashboard.component';
 import { AllOrdersComponent } from './inventory/orders/all-orders/all-orders.component';
 import { PendingOrdersComponent } from './inventory/orders/pending-orders/pending-orders.component';
 import { CompleteOrdersComponent } from './inventory/orders/complete-orders/complete-orders.component';
+import { SideNavComponent } from './inventory/side-nav/side-nav.component';
+import { CustomerComponent } from './inventory/customer/customer.component';
+import { SupplierComponent } from './inventory/supplier/supplier.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,10 @@ import { CompleteOrdersComponent } from './inventory/orders/complete-orders/comp
     DashboardComponent,
     AllOrdersComponent,
     PendingOrdersComponent,
-    CompleteOrdersComponent
+    CompleteOrdersComponent,
+    SideNavComponent,
+    CustomerComponent,
+    SupplierComponent
   ],
   imports: [
     BrowserModule,
